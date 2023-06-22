d28f2a19a222f4fc736b0c0277d70386c38d0468
WYNDFURY <lecocqflorent@outlook.fr>
2023-06-22 03:57:49 +0200

    laptop layout, start smartphone
    un peu foireux, mauvaise pratique de travail

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     container: {
       center: true,
     },
+    fontFamily: {
+      'primary': "url('../fonts/Raleway-regular.ttf')",
+    },
+    extend: {
+      backgroundImage: {
+        'pc': "url('../img/screen-bg.jpg')",
+        'smartphone': "url('../img/smartphone-bg.jpg')",
+      },
+      colors: {
+        'darkblue': "#090613",
+        'lightpurple': "#7077B2",
+  
+      }
+    },
+    
   },
   plugins: [],
 }
